c2ca07f68458046c9968e7f0dd676570788e4360
light <849189490@qq.com>
2020-07-23 20:25:18 +0800

    '基本目录‘

---
+++ src/components/context/maintabbar/MainTabbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <router-view/>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img src="~assets/image/tabbar/home.png" slot="item-icon">
+        <img src="~assets/image/tabbar/home2.png" slot="item-icon-active">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img src="~assets/image/tabbar/order.png" slot="item-icon">
+        <img src="~assets/image/tabbar/order2.png" slot="item-icon-active">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img src="~assets/image/tabbar/account.png" slot="item-icon">
+        <img src="~assets/image/tabbar/account2.png" slot="item-icon-active">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img src="~assets/image/tabbar/my.png" slot="item-icon">
+        <img src="~assets/image/tabbar/my2.png" slot="item-icon-active">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from 'components/common/tabbar/TabBar.vue'
+  import TabBarItem from 'components/common/tabbar/TabBarItem.vue'
+  export default {
+    name: 'MainTabbar',
+    components:{
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Home from 'views/home/Home.vue'
+import Category from 'views/category/Category.vue'
+import Profile from 'views/profile/Profile.vue'
+import Cart from 'views/cart/Cart.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [
+	{
+		path:'',
+		redirect:'/home'
+	},
+	{
+		path: '/home',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/category',
+		name: 'Category',
+		component: Category
+	},
+	{
+		path: '/profile',
+		name: 'Profile',
+		component: Profile
+	},
+	{
+		path: '/cart',
+		name: 'Cart',
+		component: Cart
+	},
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
